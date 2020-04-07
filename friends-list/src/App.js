45c6f580152e3f275753be7a3c2282bd5139a26b
DongPhamVan <dongphamvan.bkap@gmail.com>
2020-04-07 15:31:48 +0700

    add person list app

---
+++ friends-list/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    { img: 22, name: "John", job: "Developer" },
+    { img: 34, name: "Bob", job: "Designer" },
+    { img: 56, name: "Perter", job: "Artist" },
+  ];
+
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]}>
+        The coronavirus COVID-19 is affecting 209 countries and territories
+        around the world and 2 international conveyances. The day is reset after
+        midnight GMT+0.
+      </Person>
+      <Person person={people[2]} />
+    </section>
+  );
+};
+
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = "https://randomuser.me/api/portraits/thumb/men/" + img + ".jpg";
+
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default App;
